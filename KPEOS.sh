5ebded78a7eed3bba9905f1681622289a99cff50
Kevin Pérez Escamilla <kpeitics@gmail.com>
2026-03-15 23:34:59 -0600

    base

---
+++ KPEOS.sh
@@ -0,0 +1,26 @@
+# System actualization
+
+echo "System Actualization"
+
+sudo pacman -Sy --noconfirm
+sudo pacman -Syu --noconfirm
+
+# Create Files
+
+# mkdir Docoments
+# mkdir Downloads
+# mkdir Images
+# mkdir Proyects
+
+# Instalations of programs
+
+# sudo pacman -S --needed git base-devel --noconfirm
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si --noconfirm
+sudo pacman -S curl --noconfirm
+yay -S neovim --noconfirm
+# yay -S oh-my-bash-git --noconfirm
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
