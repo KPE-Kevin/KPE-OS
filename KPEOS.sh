c4322e47571110aab77756f61392cbc193fd7cd2
Kevin Pérez Escamilla <kpeitics@gmail.com>
2026-03-16 22:20:52 -0600

    Change

--- KPEOS.sh
+++ KPEOS.sh
@@ -29,16 +29,17 @@ sudo pacman -Syu --noconfirm
 #yay -S neovim --noconfirm
 #yay -S hyprland --noconfirm
 #yay -S fastfetch --noconfirm
-#yay -S brave-bin --noconfirm
+yay -S brave-bin --noconfirm
 #yay -S kitty --noconfirm
 #yay -S nautilus --noconfirm
 #yay -S visual-studio-code-bin --noconfirm
 #yay -S figma-linux --noconfirm
-
 #yay -S waybar --noconfirm
+yay -S google-chrome --noconfirm
 
 # Backups of files config programs
 #cp ~/.config/fastfetch/config.json Proyects/KPEOS/backup/fastfectch
 #cp -r /etc/xdg/waybar/ Proyects/KPEOS/backup/waybar/
+#cp -r ~/.config/hypr/ Proyects/KPEOS/backup/hyprland/
 #Ejecuciòn de entornos
 #hyprland
