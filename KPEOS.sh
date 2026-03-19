3160ad2b0c98813b3e9383e3b70784be9d3aa72b
Kevin Pérez Escamilla <kpeitics@gmail.com>
2026-03-18 22:24:37 -0600

    change

--- KPEOS.sh
+++ KPEOS.sh
@@ -1,46 +1,70 @@
 # System actualization
 
-echo "System Actualization"
+# echo "System Actualization"
 
-sudo pacman -Sy --noconfirm
-sudo pacman -Syu --noconfirm
+# sudo pacman -Sy --noconfirm
+# sudo pacman -Syu --noconfirm
 
 # Create backup 
 
-#mkdir backup
-#mkdir backup/fastfetch
-#mkdir backup/hyprland
-#mkdir backup/waybar
-#mkdir backup/kitty
+# mkdir backup
+# mkdir backup/fastfetch
+# mkdir backup/hyprland
+# mkdir backup/waybar
+# mkdir backup/kitty
 # Create Files
 
-#mkdir ~/Documents
-#mkdir ~/Downloads
-#mkdir ~/Images
-#mkdir ~/Proyects
+# mkdir ~/Documents
+# mkdir ~/Downloads
+# mkdir ~/Images
+# mkdir ~/Proyects
 
 # Instalations of programs
 
-#sudo pacman -S --needed git base-devel --noconfirm
-#git clone https://aur.archlinux.org/yay.git
-#cd yay
-#makepkg -si --noconfirm
-#sudo pacman -S curl --noconfirm
-#yay -S neovim --noconfirm
-#yay -S hyprland --noconfirm
-#yay -S fastfetch --noconfirm
-#yay -S brave-bin --noconfirm
-#yay -S kitty --noconfirm
-#yay -S nautilus --noconfirm
-#yay -S visual-studio-code-bin --noconfirm
-#yay -S figma-linux --noconfirm
-#yay -S waybar --noconfirm
-#yay -S google-chrome --noconfirm
-#yay -S hyprlauncher --noconfirm
+# sudo pacman -S --needed git base-devel --noconfirm
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si --noconfirm
+# sudo pacman -S curl --noconfirm
+# yay -S neovim --noconfirm
+# yay -S hyprland --noconfirm
+# yay -S fastfetch --noconfirm
+# yay -S brave-bin --noconfirm
+# yay -S kitty --noconfirm
+# yay -S nautilus --noconfirm
+# yay -S visual-studio-code-bin --noconfirm
+# yay -S waybar --noconfirm
+# yay -S google-chrome --noconfirm
+# yay -S hyprlauncher --noconfirm
+# yay -S nerd-fonts-git --noconfirm
+
+# Prueba de audio
+# yay -S pipewire-audio --noconfirm
+# yay -S pipewire-alsa --noconfirm
+# yaya -S alsa-lib --noconfirm
+# yay -S sof-firmware --noconfirm
+# yay -S linux-firmware-cirrus --noconfirm
+# yay -S alsa-firmware --noconfirm
+# yay -S alsa-utils --noconfirm
+
+
 
 # Backups of files config programs
-#cp ~/.config/fastfetch/config.json Proyects/KPEOS/backup/fastfectch
-#cp -r /etc/xdg/waybar/ Proyects/KPEOS/backup/waybar/
-cp -r ~/.config/hypr/ Proyects/KPEOS/backup/hyprland/
-#Ejecuciòn de entornos
-#hyprland
+# cp ~/.config/fastfetch/config.json Proyects/KPEOS/backup/fastfectch
+# cp -r /etc/xdg/waybar/ Proyects/KPEOS/backup/waybar/
+# cp -r ~/.config/hypr/ Proyects/KPEOS/backup/hyprland/
+
+
+#Change configurations of enviroment
+
+cp -r ./config/fastfetch/. ~/.config/fastfetch/
+sudo cp -r ./config/waybar/. /etc/xdg/waybar/ 
+
+# pkill waybar || waybar
+
+# Personal programs
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+# \. "$HOME/.nvm/nvm.sh"
+# nvm install 24
+# yay -S figma-linux --noconfirm
+# yay -S obsidian-bin --noconfirm
