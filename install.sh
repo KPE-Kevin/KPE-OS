2ab7d4badc8301f5563ac3536c5dde5ea90b1da9
Kevin Pérez Escamilla <kpeitics@gmail.com>
2026-03-22 19:26:04 -0600

    File install

---
+++ install.sh
@@ -0,0 +1,8 @@
+loadkeys es
+systemctl enable dhcpcd
+systemctl start dhcpcd
+cat /sys/firmware/efi/fw_platform_size
+hostnamectl set-hostname "PC-1"
+timedatectl
+timedatectl set-timezone America/Mexico_City
+fdisk -l
